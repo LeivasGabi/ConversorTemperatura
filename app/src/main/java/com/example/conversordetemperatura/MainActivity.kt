0503429b9e9aa63f573b29cc2d0b85feec227f73
LeivasGabi <leivasgabi@gmail.com>
2022-12-21 15:33:56 -0300

    refatorado

--- app/src/main/java/com/example/conversordetemperatura/MainActivity.kt
+++ app/src/main/java/com/example/conversordetemperatura/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.conversordetemperatura
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.conversordetemperatura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,36 +11,40 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        var view = binding.root
+        viewBinding()
+        onClickCalculate()
+    }
 
-        setContentView(view)
-
+    private fun onClickCalculate() {
         binding.button.setOnClickListener {
-            converteTemperatura()
+            calculateTemperature()
         }
     }
-    fun converteTemperatura(){
 
-        var valor = binding.valor.text.toString().toFloat()
+    private fun viewBinding(){
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
 
-        var temperatura: Boolean = binding.temperatura.isSelected
+    private fun calculateTemperature(){
 
+        val value = binding.value.text.toString().toFloat()
 
-       when (temperatura){
+        when (binding.temperature.isSelected){
 
-        binding.cParaF.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format((valor * 1.8)+ 32 ) // celsius para fahrenheit
-        binding.cParaK.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format(valor+273.15) // celsius para kelvin
-        binding.kParaF.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format((1.8*(valor-273))+32) // kelvin para fahrenheit
-        binding.kParaC.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format(valor - 273.15) // kelvin para celsius
-        binding.fParaC.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format((valor/ 1.8) + 32) // fahrenheit celsius
-        binding.fParaK.isSelected
-            -> binding.resultado.text = "Resultado: %.2f".format((valor + 459.67)*5/9) // fahrenheit para kelvin
+        binding.celsiusForFahrenheit.isSelected
+            -> binding.result.text = "Resultado: %.2f".format((value * 1.8)+ 32 ) // celsius para fahrenheit
+        binding.celsiusForKelvin.isSelected
+            -> binding.result.text = "Resultado: %.2f".format(value+273.15) // celsius para kelvin
+        binding.kelvinForFahrenheit.isSelected
+            -> binding.result.text = "Resultado: %.2f".format((1.8*(value-273))+32) // kelvin para fahrenheit
+        binding.kelvinForCelsius.isSelected
+            -> binding.result.text = "Resultado: %.2f".format(value - 273.15) // kelvin para celsius
+        binding.fahrenheitForCelsius.isSelected
+            -> binding.result.text = "Resultado: %.2f".format((value/ 1.8) + 32) // fahrenheit celsius
+        binding.fahrenheitForKelvin.isSelected
+            -> binding.result.text = "Resultado: %.2f".format((value + 459.67)*5/9) // fahrenheit para kelvin
 
            else -> {
                println("Error")
